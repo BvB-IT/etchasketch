a3af5bdb48b5a3dddccd470e18301b65bd48a1a1
BvB-tech <jacktechfora@proton.me>
2023-08-28 12:28:11 +0200

    first setup monday

---
+++ jscript/etchasketch.js
@@ -0,0 +1,32 @@
+function createDivElement(text) {
+  const div = document.createElement("div");
+
+  if (text) div.innerText = text;
+
+  return div;
+}
+
+function createSquare() {
+  const squareText = createDivElement(["col-1"], "This is a Square");
+  const row = createDivElement(["row"]);
+  row.appendChild(squareText);
+
+  const squareBody = createDivElement(["square-body"]);
+  squareBody.appendChild(row);
+
+  //   const square = new DocumentFragment();
+  //   return square;
+}
+
+const fragment = new DocumentFragment();
+
+for (let i = 0; i < 16; i++) {
+  const el = createSquare();
+  fragment.appendChild(el);
+}
+
+document.querySelector("#maingrid").appendChild(fragment);
+
+// maingrid.style.display = "grid";
+// maingrid.style.gridTemplateColumns = "auto auto aut auto auto auto auto auto";
+// maingrid.style.gridTemplateRows = "auto auto auto auto auto auto auto auto";
