64d2ad34335c6337af7c9ab64091745e1a975377
BvB-tech <jacktechfora@proton.me>
2023-09-06 15:12:40 +0200

    woensdag

--- jscript/etchasketch.js
+++ jscript/etchasketch.js
@@ -1,54 +1,34 @@
-let userSelection;
-let gridSize;
+document.getElementById(".choose button");
+document.addEventListener("click", createGrid);
 
-function userChoice(gridSize) {
-  userSelection = prompt("Give desired grid-size here", "gridSize");
-  userSelection = gridSize;
+function userChoice() {
+  let userSelection = prompt(
+    "Give desired grid-size between 0 and 100 here",
+    "gridSize"
+  );
+  userSelection = Number(userSelection);
+  let gridSize = userSelection;
   return gridSize;
 }
 
-userChoice();
-
-const grid = document.querySelector(".maingrid");
-
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 4; j++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    grid.appendChild(cell);
+function createGrid() {
+  let create = userChoice();
+  if (create < 0 || create > 100) {
+    alert("Not a number between 0 and 100");
+    location.reload();
+  }
+  document.getElementById(
+    "maingrid"
+  ).style.gridTemplateColumns = `repeat(${create}, 1fr)`;
+  document.getElementById(
+    "maingrid"
+  ).style.gridTemplateRows = `repeat(${create}, 1fr)`;
+  const grid = document.querySelector(".maingrid");
+  for (let i = 0; i <= create; i++) {
+    for (let j = 0; j <= create; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      grid.appendChild(cell);
+    }
   }
 }
-
-// function createDivElement(text) {
-//   const div = document.createElement("div");
-
-//   if (text) div.innerText = text;
-
-//   return div;
-// }
-
-// function createSquare() {
-//   const squareText = createDivElement(["col-1"], "This is a Square");
-//   const row = createDivElement(["row"]);
-//   row.appendChild(squareText);
-
-//   const squareBody = createDivElement(["square-body"]);
-//   squareBody.appendChild(row);
-
-//   //   const square = new DocumentFragment();
-//   //   return square;
-// }
-
-// const fragment = new DocumentFragment();
-
-// for (let i = 0; i < 16; i++) {
-//   const el = createSquare();
-//   fragment.appendChild(el);
-// }
-
-// document.querySelector("#maingrid").appendChild(fragment);
-
-// maingrid.style.display = "grid";
-// maingrid.style.gridTemplateColumns = "auto auto aut auto auto auto auto auto";
-// maingrid.style.gridTemplateRows = "auto auto auto auto auto auto auto auto";
-// https://stackoverflow.com/questions/60697428/creating-multiple-elements-with-javascript
